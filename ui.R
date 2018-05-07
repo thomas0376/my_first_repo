ec4858351f8ade030d5ac67379534dcbb45fc42e
Thomas Prevarin <thomasprevarin@Thomass-MBP.lan>
2018-05-07 16:45:17 +0100

    changed default for text input wirget

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ shiny::fluidPage(
   shiny::sidebarLayout(
     
     shiny::sidebarPanel(
-      shiny::textInput("title", "Plot title", "Car speed vs distance to stop")
+      shiny::textInput("title", "Plot title", "give me a title")
       
     ),
     
